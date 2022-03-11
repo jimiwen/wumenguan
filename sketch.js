d6499528232027b97c8e87bc5e1103ed4cd1b413
Jimi Wen <jimi.wen@gmail.com>
2022-03-11 17:52:01 +0800

    composition 3

--- sketch.js
+++ sketch.js
@@ -150,50 +150,60 @@ let yaoL=[0 ,0 ,0];
 }
 
 
-coloring={
-  color1: '#bfb8c2',
-  color2: '#d0dbd8',
-  color3: '#c2740c',
-}
 
-if (seasonpicker<0.25){
-  //pink 35%
+if (seasonpicker<0.4){
+
+  // saturated
   colors = {
-    main: "#423b40",
-    bg: "#02040a",
-    sea1: '#0892c4',
-    sea2: '#02c0d9',
-    sand1:'#635c61',
-    sand2:'#52483c',
-    body2:'#069c94',
-    body1:'#64faf3'
-  };
-  coloring={
-    color1: '#eb528d',
-    color2: '#89c7c3',
-    color3: '#f5965f',
-  }
-}else if (seasonpicker<0.5){
-  //summer green 5%
-  colors = {
-    main: "#f7dfdf",
-    bg: "#692545",
-    sea1: '#bd112b',
-    sea2: '#de4573',
-    sand1:'#3c4d3d',
-    sand2:'#2d4038',
-    body2:'#f07832',
-    body1:'#edaf3b'
+    main: "#375093",
+    bg: "#aea9b0",
+    sea1: '#f7e623',
+    sea2: '#25702a',
+    sand1:'#3f4c66',
+    sand2:'#d95a0b',
+    body2:'#fd4661',
+    body1:'#2f68ad'
   };
 
-  coloring={
-    color1: '#08ffb5',
-    color2: '#280096',
-    color3: '#c2740c',
+  // // baby blue baby purple
+  // colors = {
+  //   main: "693793",
+  //   bg: "#5323d1",
+  //   sea1: '#c892c4',
+  //   sea2: '#6fc0d9',
+  //   sand1:'#d35c61',
+  //   sand2:'#51423c',
+  //   body2:'#623c94',
+  //   body1:'#5ffaf3'
+  // };
 
-  }
-}else if (seasonpicker<0.75){
-  // golden tiger 10%
+  // // baby blue baby pink
+  // colors = {
+  //   main: "#932793",
+  //   bg: "#93lsdj",
+  //   sea1: '#f892c4',
+  //   sea2: '#0fc0d9',
+  //   sand1:'#f35c61',
+  //   sand2:'#f2483c',
+  //   body2:'#f69c94',
+  //   body1:'#f4faf3'
+  // };
+
+
+  // //aqua
+  // colors = {
+  //   main: "#423b40",
+  //   bg: "#02040a",
+  //   sea1: '#0892c4',
+  //   sea2: '#02c0d9',
+  //   sand1:'#635c61',
+  //   sand2:'#52483c',
+  //   body2:'#069c94',
+  //   body1:'#64faf3'
+  // };
+
+}else if (seasonpicker<0.45){
+  //golden tiger 10%
   colors = {
     main: "#ff7700",
     bg: "#ff8800",
@@ -205,36 +215,60 @@ if (seasonpicker<0.25){
     body1:'#ff8800',
   };
 
-  coloring={
-    color1:'#2f1a38',
-  	color2:'#88898c',
-  	color3:'#404759',
-}
+  //summer green 5%
+  // colors = {
+  //   main: "#f7dfdf",
+  //   bg: "#692545",
+  //   sea1: '#bd112b',
+  //   sea2: '#de4573',
+  //   sand1:'#3c4d3d',
+  //   sand2:'#2d4038',
+  //   body2:'#f07832',
+  //   body1:'#edaf3b'
+  // };
+
+  // baby blue baby purple
+  // colors = {
+  //   main: "693793",
+  //   bg: "#5323d1",
+  //   sea1: '#c892c4',
+  //   sea2: '#6fc0d9',
+  //   sand1:'#d35c61',
+  //   sand2:'#51423c',
+  //   body2:'#623c94',
+  //   body1:'#5ffaf3'
+  // };
+
+}else if (seasonpicker<0.60){
+
+
+  // baby blue baby pink
+  colors = {
+    main: "#932793",
+    bg: "#93lsdj",
+    sea1: '#f892c4',
+    sea2: '#0fc0d9',
+    sand1:'#f35c61',
+    sand2:'#f2483c',
+    body2:'#f69c94',
+    body1:'#f4faf3'
+  };
+
 
 }else{
   //50% winter
   colors = {
     main: "#02040a",
-    bg: "#f1f1f1",
-    sea1: '#0892c4',
-    sea2: '#02c0d9',
-    sand1:'#02040a',
-    sand2:'#52483c',
-    body2:'#069c94',
-    body1:'#64faf3'
+    bg: "#93b9ba",
+    sea1: '#199ecf',
+    sea2: '#1f1001',
+    sand1:'#bd6e2a',
+    sand2:'#49c9a9',
+    body2:'#cfc508',
+    body1:'#d99873'
   };
 
-  coloring={
-    color1: '#bfb8c2',
-    color2: '#d0dbd8',
-    color3: '#c2740c',
-  }
 }
-
-
-
-
-
 
 
 function setup() {
@@ -471,455 +505,7 @@ function yao2gua(yaoyao){
   }else return "kan"
 }
 
-function drawFlower(coloring,thick,y1){
-  //console.log(y1)
-  offset=random(50,100);
-  framepoints=[
-    [0,0],
-    [0,3000],
-    [3000,3000],
-    // [3000,y1+offset],
-    // [0,y1+offset],
-    // [0,y1],
-    // [3000,y1],
-    [3000,0],
-    [0,0]
-  ];
-  rc.polygon(framepoints, {
-    fill: random([colors.body2,colors.body1,colors.sea2,colors.sea1,colors.sand2]),
-    fillStyle: random(["hachure"]),
-    fillWeight: thick*random(0.05,0.1),
-    hachureAngle: random(0,180),
-    stroke: "transparent",
-  });
-  rc.polygon(framepoints, {
-    fill: random([colors.sand1,colors.sand1,colors.sea2,colors.sea1,colors.sand2]),
-    fillStyle: random(["hachure"]),
-    fillWeight: thick*random(0.05,0.1),
-    hachureAngle: random(0,180),
-    stroke: "transparent",
-  });
-  rc.polygon(framepoints, {
-    fill: random([colors.sand1,colors.sand1,colors.sea2,colors.sea1,colors.sand2]),
-    fillStyle: random(["hachure"]),
-    fillWeight: thick*random(0.05,0.1),
-    hachureAngle: random(0,180),
-    stroke: "transparent",
-  });
 
-  //
-  // rc.polygon(framepoints, {
-  //   fill: random([colors.sand1,colors.sand1,colors.sand1,colors.sand1,colors.sand2]),
-  //   fillStyle: random(["hachure"]),
-  //   fillWeight: thick*random(0.11,0.2),
-  //   hachureAngle: random(0,180),
-  //   stroke: "transparent",
-  // });
-  // rc.polygon(framepoints, {
-  //   fill: random([colors.sand1,colors.sand1,colors.sand1,colors.sand1,colors.sand2]),
-  //   fillStyle: random(["hachure"]),
-  //   fillWeight: thick*random(0.11,0.2),
-  //   hachureAngle: random(0,180),
-  //   stroke: "transparent",
-  // });
-  // rc.polygon(framepoints, {
-  //   fill: random([colors.sand1,colors.sand1,colors.sand1,colors.sand2,colors.sand2]),
-  //   fillStyle: random(["hachure"]),
-  //   fillWeight: thick*random(0.11,0.2),
-  //   hachureAngle: random(0,180),
-  //   stroke: "transparent",
-  // });
-  // rc.polygon(framepoints, {
-  //   fill: random([colors.sand1,colors.sand1,colors.sand1,colors.sand2,colors.sand2]),
-  //   fillStyle: random(["hachure"]),
-  //   fillWeight: thick*random(0.11,0.2),
-  //   hachureAngle: random(0,180),
-  //   stroke: "transparent",
-  // });
-
-}
-//
-function drawFrames(coloring,thick){
-
-
-  tiandi=random([4,9]);
-  shengchengX=ceil(random(0,tiandi+0.00001))-1;
-
-
-  shengchengY=ceil(random(0,tiandi+0.00001))-1;
-
-  vertpos1a=[
-    fullgua[tiandi][shengchengX][0],
-    fullgua[tiandi][shengchengY][1],
-  ]
-
-  tiandi=random([4,9]);
-  shengchengX=ceil(random(0,tiandi+0.00001))-1;
-
-
-  shengchengY=ceil(random(0,tiandi+0.00001))-1;
-
-  vertpos2a=[
-    fullgua[tiandi][shengchengX][0],
-    fullgua[tiandi][shengchengY][1],
-  ]
-
-  tiandi=random([4,9]);
-  shengchengX=ceil(random(0,tiandi+0.00001))-1;
-
-
-  shengchengY=ceil(random(0,tiandi+0.00001))-1;
-
-  vertpos3a=[
-    fullgua[tiandi][shengchengX][0],
-    fullgua[tiandi][shengchengY][1],
-  ]
-
-  tiandi=random([4,9]);
-  shengchengX=ceil(random(0,tiandi+0.00001))-1;
-
-
-  shengchengY=ceil(random(0,tiandi+0.00001))-1;
-
-  vertpos4a=[
-    fullgua[tiandi][shengchengX][0],
-    fullgua[tiandi][shengchengY][1],
-  ]
-
-
-  framepointRandom=[
-    //  [random([0,300,800,2200,2800,3000]),random([0,300,800,2200,2800,3000])],
-    // vertpos1=[tianyishengshui[0][0],tianyishengshui[0][1]],
-    // vertpos2=[diershenghuo[0][0],diershenghuo[0][1]],
-    // vertpos3=[dishichengtu[0][0],dishichengtu[0][1]],
-    // vertpos4=[dibachengmu[6][0],dibachengmu[6][1]],
-
-    vertpos1,
-    vertpos1a,
-    vertpos2,
-    vertpos2a,
-    vertpos3,
-    vertpos3a,
-    vertpos4,
-    vertpos4a,
-    //  [random([0,300,800,2200,2800,3000]),random([0,300,800,2200,2800,3000])],
-  ];
-
-  rc.polygon(framepointRandom, {
-    fill: coloring,
-    fillStyle: random(["hachure"]),
-    fillWeight: thick*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",
-  });
-  rc.polygon(framepointRandom, {
-    fill: coloring,
-    fillStyle: random(["hachure"]),
-    fillWeight: thick*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",
-  });
-  rc.polygon(framepointRandom, {
-    fill: coloring,
-    fillStyle: random(["hachure"]),
-    fillWeight: thick*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",
-  });
-  // rc.polygon(framepointRandom, {
-  //   fill: coloring,
-  //   fillStyle: random(["hachure"]),
-  //   fillWeight: thick*random(0.11,0.3),
-  //   hachureAngle: random(0,180),
-  //   stroke: "transparent",
-  // });
-  // rc.polygon(framepointRandom, {
-  //   fill: coloring,
-  //   fillStyle: random(["hachure"]),
-  //   fillWeight: thick*random(0.11,0.3),
-  //   hachureAngle: random(0,180),
-  //   stroke: "transparent",
-  // });
-}
-
-
-
-
-
-function drawhetu(){
-  raddd=600;
-  chengtu_centre=[1800,3200];
-  dishichengtu=[
-    [chengtu_centre[0]+raddd*cos(1*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(1*2*PI/10)],
-    [chengtu_centre[0]+raddd*cos(2*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(2*2*PI/10)],
-    [chengtu_centre[0]+raddd*cos(3*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(3*2*PI/10)],
-    [chengtu_centre[0]+raddd*cos(4*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(4*2*PI/10)],
-    [chengtu_centre[0]+raddd*cos(5*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(5*2*PI/10)],
-    [chengtu_centre[0]+raddd*cos(6*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(6*2*PI/10)],
-    [chengtu_centre[0]+raddd*cos(7*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(7*2*PI/10)],
-    [chengtu_centre[0]+raddd*cos(8*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(8*2*PI/10)],
-    [chengtu_centre[0]+raddd*cos(9*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(9*2*PI/10)],
-    [chengtu_centre[0]+raddd*cos(10*2*PI/10),chengtu_centre[1]+1.8*raddd*sin(10*2*PI/10)],
-  ];
-
-  tianwushengtu=[
-    [chengtu_centre[0]-300,chengtu_centre[1]],
-    [chengtu_centre[0],chengtu_centre[1]-500],
-    [chengtu_centre[0],chengtu_centre[1]],
-    [chengtu_centre[0]+300,chengtu_centre[1]],
-    [chengtu_centre[0],chengtu_centre[1]+500],
-  ];
-
-  tianyishengshui=[
-    [chengtu_centre[0],chengtu_centre[1]+2000]
-  ];
-
-  widsix=500;
-bottom_edge=2800;
-  diliuchengshui=[
-    [chengtu_centre[0]-2.5*widsix,chengtu_centre[1]+bottom_edge],
-    [chengtu_centre[0]-1.5*widsix,chengtu_centre[1]+bottom_edge],
-    [chengtu_centre[0]-0.5*widsix,chengtu_centre[1]+bottom_edge],
-    [chengtu_centre[0]+0.5*widsix,chengtu_centre[1]+bottom_edge],
-    [chengtu_centre[0]+1.5*widsix,chengtu_centre[1]+bottom_edge],
-    [chengtu_centre[0]+2.5*widsix,chengtu_centre[1]+bottom_edge],
-  ];
-
-left_edge=200;
-wid8=370;
-  dibachengmu=[
-    [left_edge,chengtu_centre[1]-7*wid8],
-    [left_edge,chengtu_centre[1]-5*wid8],
-    [left_edge,chengtu_centre[1]-3*wid8],
-    [left_edge,chengtu_centre[1]-wid8],
-    [left_edge,chengtu_centre[1]+wid8],
-    [left_edge,chengtu_centre[1]+3*wid8],
-    [left_edge,chengtu_centre[1]+5*wid8],
-    [left_edge,chengtu_centre[1]+7*wid8],
-  ];
-
-  tiansanshengmu=[
-    [left_edge+400,chengtu_centre[1]-4.6*wid8],
-    [left_edge+400,chengtu_centre[1]],
-    [left_edge+400,chengtu_centre[1]+4.6*wid8],
-  ];
-wid6=450;
-  tianqichenghuo=[
-    [chengtu_centre[0]-3*wid6,200],
-    [chengtu_centre[0]-2*wid6,200],
-    [chengtu_centre[0]-wid6,200],
-    [chengtu_centre[0],200],
-    [chengtu_centre[0]+wid6,200],
-    [chengtu_centre[0]+2*wid6,200],
-    [chengtu_centre[0]+3*wid6,200],
-  ];
-
-  diershenghuo=[
-    [chengtu_centre[0]-400,801],
-    [chengtu_centre[0]+400,801]
-  ];
-  widnine=660
-  right_edge=3400
-  tianjiuchengjin=[
-    [right_edge,chengtu_centre[1]-4*widnine],
-    [right_edge,chengtu_centre[1]-3*widnine],
-    [right_edge,chengtu_centre[1]-2*widnine],
-    [right_edge,chengtu_centre[1]-1*widnine],
-    [right_edge,chengtu_centre[1]],
-    [right_edge,chengtu_centre[1]+1*widnine],
-    [right_edge,chengtu_centre[1]+2*widnine],
-    [right_edge,chengtu_centre[1]+3*widnine],
-    [right_edge,chengtu_centre[1]+4*widnine],
-  ];
-
-  disishengjin=[
-    [right_edge-widnine,chengtu_centre[1]-2.4*widnine],
-    [right_edge-widnine,chengtu_centre[1]-0.8*widnine],
-    [right_edge-widnine,chengtu_centre[1]+0.8*widnine],
-    [right_edge-widnine,chengtu_centre[1]+2.4*widnine],
-  ];
-
-  rc.polygon(dishichengtu,{
-    fill: coloring.color1,
-    fillStyle: random(["hachure"]),
-    fillWeight: 2*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",
-  }
-)
-
-fill(coloring.color1)
-for (k=0;k<10;k++){
-  rc.circle(dishichengtu[k][0],dishichengtu[k][1],60,{
-    fill: coloring.color1,
-    fillStyle: random(["hachure"]),
-    fillWeight: 4*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",}
-  )
-}
-
-fill(colors.sand1)
-for (k=0;k<10;k++){
-  rc.circle(dishichengtu[k][0],dishichengtu[k][1],50,{
-    fill: colors.sand1,
-    fillStyle: random(["hachure"]),
-    fillWeight: 4*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",}
-  )
-}
-
-fill(colors.sand1)
-for (k=0;k<5;k++){
-  rc.circle(tianwushengtu[k][0],tianwushengtu[k][1],50,{
-    fill: colors.sand1,
-    fillStyle: random(["hachure"]),
-    fillWeight: 4*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",}
-  )
-}
-
-fill(coloring.color1)
-for (k=0;k<1;k++){
-  rc.circle(tianyishengshui[k][0],tianyishengshui[k][1],60,{
-    fill: coloring.color1,
-    fillStyle: random(["hachure"]),
-    fillWeight: 4*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",}
-  )
-}
-
-fill(colors.sand1)
-for (k=0;k<1;k++){
-  rc.circle(tianyishengshui[k][0],tianyishengshui[k][1],50,{
-    fill: colors.sand1,
-    fillStyle: random(["hachure"]),
-    fillWeight: 4*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",}
-  )
-}
-
-fill(coloring.color1)
-for (k=0;k<6;k++){
-  rc.circle(diliuchengshui[k][0],diliuchengshui[k][1],50,{
-    fill: coloring.color1,
-    fillStyle: random(["hachure"]),
-    fillWeight: 4*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",}
-  )
-}
-
-
-fill(coloring.color1)
-for (k=0;k<8;k++){
-  rc.circle(dibachengmu[k][0],dibachengmu[k][1],50,{
-    fill: coloring.color1,
-    fillStyle: random(["hachure"]),
-    fillWeight: 4*random(0.11,0.3),
-    hachureAngle: random(0,180),
-    stroke: "transparent",}
-  )
-}
-
-
-fill(coloring.color1)
-for (k=0;k<3;k++){
-  rc.circle(tiansanshengmu[k][0],tiansanshengmu[k][1],60,
-    {
-      fill: coloring.color1,
-      fillStyle: random(["hachure"]),
-      fillWeight: 4*random(0.11,0.3),
-      hachureAngle: random(0,180),
-      stroke: "transparent",})
-}
-
-fill(colors.sand1)
-for (k=0;k<3;k++){
-  rc.circle(tiansanshengmu[k][0],tiansanshengmu[k][1],50,
-    {
-      fill: colors.sand1,
-      fillStyle: random(["hachure"]),
-      fillWeight: 8*random(0.11,0.3),
-      hachureAngle: random(0,180),
-      stroke: "transparent",})
-}
-
-fill(coloring.color1)
-for (k=0;k<2;k++){
-  rc.circle(diershenghuo[k][0],diershenghuo[k][1],50,
-    {
-      fill: coloring.color1,
-      fillStyle: random(["hachure"]),
-      fillWeight: 4*random(0.11,0.3),
-      hachureAngle: random(0,180),
-      stroke: "transparent",})
-}
-
-
-fill(coloring.color1)
-for (k=0;k<7;k++){
-  rc.circle(tianqichenghuo[k][0],tianqichenghuo[k][1],60,
-    {
-      fill: coloring.color1,
-      fillStyle: random(["hachure"]),
-      fillWeight: 4*random(0.11,0.3),
-      hachureAngle: random(0,180),
-      stroke: "transparent",})
-}
-
-fill(colors.sand1)
-for (k=0;k<7;k++){
-  rc.circle(tianqichenghuo[k][0],tianqichenghuo[k][1],50,
-    {
-      fill: colors.sand1,
-      fillStyle: random(["hachure"]),
-      fillWeight: 8*random(0.11,0.3),
-      hachureAngle: random(0,180),
-      stroke: "transparent",})
-}
-
-
-fill(coloring.color1)
-for (k=0;k<4;k++){
-  rc.circle(disishengjin[k][0],disishengjin[k][1],50,
-    {
-      fill: coloring.color1,
-      fillStyle: random(["hachure"]),
-      fillWeight: 4*random(0.11,0.3),
-      hachureAngle: random(0,180),
-      stroke: "transparent",})
-}
-
-
-fill(coloring.color1)
-for (k=0;k<9;k++){
-  rc.circle(tianjiuchengjin[k][0],tianjiuchengjin[k][1],60,
-    {
-      fill: coloring.color1,
-      fillStyle: random(["hachure"]),
-      fillWeight: 4*random(0.11,0.3),
-      hachureAngle: random(0,180),
-      stroke: "transparent",})
-}
-
-fill(colors.sand1)
-for (k=0;k<9;k++){
-  rc.circle(tianjiuchengjin[k][0],tianjiuchengjin[k][1],50,
-    {
-      fill: colors.sand1,
-      fillStyle: random(["hachure"]),
-      fillWeight: 8*random(0.11,0.3),
-      hachureAngle: random(0,180),
-      stroke: "transparent",})
-}
-
-// console.log(tianyishengshui)
-}
 //     License : Copyright (C) 2022 Jimi Y. C. Wen . All rights reserved.\n
 //     Licensed under CC BY-NC-SA 4.0
 //    https://github.com/jimiwen
